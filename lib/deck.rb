45a009e09c89c660b116e732129e59e1c67a2226
Mills Provosty <millsprovosty@gmail.com>
2019-01-26 14:53:23 -0700

    working on id'ing category

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,8 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
 
 class Deck
-  attr_reader :cards, :count
-  def initialize(cards, count)
+attr_reader :cards, :count, :category
+  def initialize(cards, count, category)
     @cards = cards
     @count = count
+    @id_card_category = []
   end
+
+def id_card_category
+     id_card_category = []
+     deck.find_all do |x|
+  #name the new object and load it
+       p card.category == x
+       x << id_category
+ end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/card'
 require './lib/deck'
 require './lib/turn'
-require "pry"
 
 class DeckTest < Minitest::Test
     def is_there_a_deck
@@ -12,6 +11,7 @@ class DeckTest < Minitest::Test
     end
 
     def cards_are_in_the_deck
+
       cards = [card_1, card_2, card_3]
         card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
         card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -19,8 +19,20 @@ class DeckTest < Minitest::Test
       assert_equal [card_1, card_2, card_3] , cards
     end
 
-   def how_many_cards_in_deck
+    def how_many_cards_in_deck
+
      count = cards.count
      assert_equal 3, cards
-   end
+    end
+
+    def count_cards_in_category_STEM
+         category = [:STEM, :Geography, "Pop Culture"]
+         deck.find_all do |cards|
+         category << deck[:STEM]
+     end
   end
+end
+
+
+require "pry"
+binding.pry
