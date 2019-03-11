69058413290ccb7fd068b18f6b61770f2e006e27
Mills Provosty <millsprovosty@gmail.com>
2019-03-11 17:53:06 -0600

    First section complete

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -14,4 +14,17 @@ class TurnTest < Minitest::Test
     @card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     @turn = Turn.new("Juneau", @card)
   end
+
+  def test_turn_has_attributes
+    assert_equal @card, @turn.card
+    assert_equal "Juneau", @turn.guess
+  end
+
+  def test_correct_returns_true_or_false
+    assert @turn.correct?
+  end
+
+  def test_feedback_returns_correct_feedback
+    assert_equal "Correct!", @turn.feedback
+  end
 end
