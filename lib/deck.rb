d7e08aaa05acf771c65bd4011dd35a984b8365d5
Mills Provosty <millsprovosty@gmail.com>
2019-03-11 18:26:47 -0600

    Deck tests and class complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,4 +3,18 @@ class Deck
   def initialize(cards)
     @cards = cards
   end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(category)
+    cards_category = []
+    @cards.map do |card|
+      if card.category == category
+        cards_category << card
+      end
+    end
+    return cards_category
+  end
 end
